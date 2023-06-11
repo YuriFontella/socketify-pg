6fcd7c7aad7001e1d3f13d297cdf559ffda60203
Yuri <yurifc4@gmail.com>
2023-06-10 21:25:09 -0300

    psycopg 3

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from socketify import App
+from psycopg.rows import dict_row
 
-import psycopg2
+import psycopg
 
-conn = psycopg2.connect("user=postgres password=123456 host=localhost dbname=blocks")
+conn = psycopg.connect("user=postgres password=123456 host=localhost dbname=blocks", row_factory=dict_row)
 
 cursor = conn.cursor()
 
 app = App()
 
 async def home(res, req):
-  cursor.execute("select * from users limit 2000000")
-  cursor.fetchall()
+  cursor.execute("select name from users limit 1")
+  records = cursor.fetchall()
 
-  res.end("socketify and psycopg2 - Python")
+  res.send(records)
 
 app.get("/", home)
 
