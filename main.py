a91f6d04c5472aa7374e7995e12c321b4f9b45a8
Yuri <yurifc4@gmail.com>
2023-06-12 10:44:43 -0300

    post

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ db = Database().conn
 
 app = App()
 
-def home(res, req):
+def get(res, req):
   records = db.execute("select name from users limit 1").fetchall()
 
   print(len(records))
 
   res.send(records)
 
-app.get("/", home)
+async def post(res, req):
+  data = await res.get_json()
+
+  try:
+
+    db.execute('insert into groups (name, total, saved) values (%(name)s, %(total)s, %(saved)s)', data)
+
+  except: res.end(False)
+
+  finally:
+
+    db.commit()
+
+    res.end(True)
+
+app.get("/", get)
+app.post("/", post)
 
 app.listen(3000)
 app.run()
